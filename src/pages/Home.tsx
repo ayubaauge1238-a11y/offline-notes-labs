72acf9a4094152dd57bb03f4461c8d79c4f94e2d
ayubaauge1238-a11y <ayubaauge1238@st.futminna.edu.ng>
2026-09-10 20:13:54 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -120,8 +120,8 @@ export default function Home() {
         <h1>Keep learning when the network leaves.</h1>
 
         <div className="author"> 
-          <p>Project By: <i>Obe Fortune Olotu</i> </p>
-          <p>Matric No: <i>2024/1/95114CP</i> </p>
+          <p>Project By: <i>Ayuba Abdulmajid</i> </p>
+          <p>Matric No: <i>2024/1/98666EE</i> </p>
 
         </div>
 
